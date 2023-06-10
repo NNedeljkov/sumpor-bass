f13fd8ce1c80f92b9932c7e2b2c19893e9c3b979
Ivan Prtljaga <38421835+ivanprtljaga@users.noreply.github.com>
2023-06-10 22:58:08 +0200

    fix: something

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,51 +2,53 @@ import React from "react";
 import styled from "styled-components";
 
 const Wrapper = styled.div`
-    height: 400px;
-    width: 250px;
+  height: 400px;
+  width: 250px;
 
-    display: flex;
-    align-items: center;
-    flex-direction: column;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 
-    padding:20px;
-    gap: 15px;
-    border: 3px solid black;
-    border-radius: 25px;
-    box-shadow: 10px 10px gray;
+  padding:20px;
+  gap: 15px;
+  border: 3px solid black;
+  border-radius: 25px;
+  box-shadow: 10px 10px gray;
 `;
 
 const Description = styled.div`
-    width: 100%;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
+  width: 100%;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
 
 const Price = styled.div`
 
 `;
 
-const Image = styled.div`
-    height: 200px;
-    width: 120px;
+const Image = styled.img`
+  height: 200px;
+  width: 120px;
+
+  object-fit: contain;
 `;
 
 const Card = ({
-    name,
-    price,
-    description,
-    notes,
-    img,
+  name,
+  price,
+  description,
+  notes,
+  img,
 }) => (
-    <Wrapper>
-        <Image src={(`${img}`)} />
-        <h3>
-            {name}
-        </h3>
-        <Price>{price}</Price>
-        <Description>{description}</Description>
-    </Wrapper>
+  <Wrapper>
+    <Image src={(`${img}`)} />
+    <h3>
+      {name}
+    </h3>
+    <Price>{price}</Price>
+    <Description>{description}</Description>
+  </Wrapper>
 )
 
 export default Card;
--- src/components/Content.js
+++ src/components/Content.js
@@ -6,50 +6,47 @@ import Card from './Card';
 import data from '../assets/data.json';
 
 const Wrapper = styled.div`
-    display: flex;
-    width: 100%;
-    max-width: 1000px;
-    height: 100%;
-    flex: 1;
-    flex-direction: column;
-    align-items: center;
+  display: flex;
+  width: 100%;
+  max-width: 1000px;
+  height: 100%;
+  flex: 1;
+  flex-direction: column;
+  align-items: center;
 `;
 
 const TitleWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 20px;
-    
-    margin-bottom: 50px
-
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  margin-bottom: 50px;
 `;
 
 const CardWrapper = styled.div`
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    align-items: center;
-    justify-content: center;
-
-    gap: 20px;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+  gap: 20px;
 `;
 
 
 const Content = () => {
 
     return (
-        <Wrapper>
-            <TitleWrapper>
-                <h1>SUMPOOOOOOOOOR</h1>
-                <h3>this is a blog about bass guitars</h3>
-            </TitleWrapper>
-            <CardWrapper>
-                {data.map( item => (
-                    <Card {...item} />
-                ))}
-            </CardWrapper>
-        </Wrapper>
+      <Wrapper>
+        <TitleWrapper>
+          <h1>SUMPOOOOOOOOOR</h1>
+          <h3>this is a blog about bass guitars</h3>
+        </TitleWrapper>
+        <CardWrapper>
+          {data.map( item => (
+              <Card {...item} />
+          ))}
+        </CardWrapper>
+      </Wrapper>
     )
 }
 
